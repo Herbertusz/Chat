195e27951fd7f71bdf2d1d3b9926ad799d4d166c
norbi <horvath.norbert@dmsone.hu>
2016-05-25 16:56:06 +0200

    User-sáv görgetés

--- app/public/js/chat/controller.js
+++ app/public/js/chat/controller.js
@@ -6,6 +6,8 @@ var CHAT = window.CHAT || {};
 
 $(document).ready(function(){
 
+	const $BOX = $(CHAT.DOM.box);
+
 	// Csatorna létrehozása
 	$(CHAT.DOM.start).click(function(){
 		CHAT.Events.Client.createRoom();
@@ -13,20 +15,20 @@ $(document).ready(function(){
 	});
 
 	// Kilépés csatornából
-	$(CHAT.DOM.box).find(CHAT.DOM.close).click(function(){
+	$BOX.find(CHAT.DOM.close).click(function(){
 		CHAT.Events.Client.leaveRoom($(this).parents(CHAT.DOM.box));
 	});
 
 	// User hozzáadása csatornához
 	$(CHAT.DOM.userSelect).change(function(){
 		if ($(CHAT.DOM.selectedUsers).length > 0){
-			$(CHAT.DOM.box).filter(':not([data-disabled])').find(CHAT.DOM.addUser).show();
+			$BOX.filter(':not([data-disabled])').find(CHAT.DOM.addUser).show();
 		}
 		else {
-			$(CHAT.DOM.box).find(CHAT.DOM.addUser).hide();
+			$BOX.find(CHAT.DOM.addUser).hide();
 		}
 	});
-	$(CHAT.DOM.box).find(CHAT.DOM.addUser).click(function(){
+	$BOX.find(CHAT.DOM.addUser).click(function(){
 		const $add = $(this);
 		$(CHAT.DOM.selectedUsers).each(function(){
 			CHAT.Events.Client.forceJoinRoom($add, Number($(this).val()));
@@ -35,12 +37,12 @@ $(document).ready(function(){
 	});
 
 	// User kidobása csatornából
-	$(CHAT.DOM.box).find(CHAT.DOM.userThrow).click(function(){
+	$BOX.find(CHAT.DOM.userThrow).click(function(){
 		CHAT.Events.Client.forceLeaveRoom($(this));
 	});
 
 	// Hibaüzenet eltüntetése
-	$(CHAT.DOM.box).find(CHAT.DOM.errorClose).click(function(){
+	$BOX.find(CHAT.DOM.errorClose).click(function(){
 		$(this).parents(CHAT.DOM.box).find(CHAT.DOM.error).hide();
 	});
 
@@ -65,7 +67,7 @@ $(document).ready(function(){
 	});
 
 	// Üzenetküldés indítása ENTER leütésére
-	$(CHAT.DOM.box).find(CHAT.DOM.message).keydown(function(event){
+	$BOX.find(CHAT.DOM.message).keydown(function(event){
 		const $box = $(this).parents('.chat');
 		if (event.which === HD.Misc.keys.ENTER){
 			if (!event.shiftKey && $box.find(CHAT.DOM.sendSwitch).prop("checked")){
@@ -76,28 +78,28 @@ $(document).ready(function(){
 	});
 
 	// Üzenetküldés indítása gombnyomásra
-	$(CHAT.DOM.box).find(CHAT.DOM.sendButton).click(function(){
+	$BOX.find(CHAT.DOM.sendButton).click(function(){
 		const $box = $(this).parents('.chat');
 		CHAT.Events.Client.sendMessage($box);
 	});
 
 	// Fájlküldés
-	$(CHAT.DOM.box).find(CHAT.DOM.fileTrigger).click(function(){
+	$BOX.find(CHAT.DOM.fileTrigger).click(function(){
 		$(this).parents(CHAT.DOM.box).find(CHAT.DOM.file).trigger("click");
 	});
-	$(CHAT.DOM.box).find(CHAT.DOM.file).change(function(){
+	$BOX.find(CHAT.DOM.file).change(function(){
 		const $box = $(this).parents('.chat');
 		const files = $box.find(CHAT.DOM.file).get(0).files;
 		if (files.length > 0){
 			CHAT.Events.Client.sendFile($box, files);
 		}
 	});
-	$(CHAT.DOM.box).on('click', 'a.notredirect', function(event){
+	$BOX.on('click', 'a.notredirect', function(event){
 		// event.preventDefault();
 	});
 
 	// Fájlküldés (drag-n-drop)
-	$(CHAT.DOM.box).find(CHAT.DOM.dropFile).on(
+	$BOX.find(CHAT.DOM.dropFile).on(
 		'drag dragstart dragend dragover dragenter dragleave drop',
 		function(event){
 			event.preventDefault();
@@ -117,7 +119,7 @@ $(document).ready(function(){
 	});
 
 	// Üzenet gépelése
-	$(CHAT.DOM.box).find(CHAT.DOM.message).keyup(function(event){
+	$BOX.find(CHAT.DOM.message).keyup(function(event){
 		const $box = $(this).parents('.chat');
 		if (event.which !== HD.Misc.keys.ENTER){
 			CHAT.Events.Client.typeMessage($box);
@@ -125,8 +127,13 @@ $(document).ready(function(){
 	});
 
 	// Üzenetküldés módja
-	$(CHAT.DOM.box).find(CHAT.DOM.sendSwitch).change(function(){
+	$BOX.find(CHAT.DOM.sendSwitch).change(function(){
 		CHAT.Events.Client.sendMethod($(this));
+	});
+
+	// User-sáv görgetése
+	$BOX.find(CHAT.DOM.users).mousedown(function(){
+		;
 	});
 
 	// Szerver által küldött események lekezelése
